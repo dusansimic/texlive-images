6b64593b7b7146fcadda6bd21c98a73a25ca6c3f
Dušan Simić <dusan.simic1810@gmail.com>
2024-05-16 18:30:45 +0200

    feat: Add all files

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+ARG IMAGE_TAG
+FROM docker.io/library/ubuntu:${IMAGE_TAG}
+
+ARG TL_VERSION
+
+COPY scripts/install_tl.sh /tmp/
+RUN TL_VERSION=${TL_VERSION} bash /tmp/install_tl.sh && rm /tmp/install_tl.sh
+ENV PATH="/usr/local/texlive/${TL_VERSION}/bin/x86_64-linux:${PATH}"
---
+++ scripts/install_tl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+TL_VERSION="$1"
+
+if [ -z "${TL_VERSION}" ]
+then
+  echo "texlive version not specified!"
+  exit 1
+fi
+
+apt-get update
+apt-get install -y perl curl
+
+pushd /tmp
+
+curl -L -o install-tl.unx.tar.gz https://texlive.info/historic/systems/texlive/${TL_VERSION}/install-tl-unx.tar.gz
+zcat < install-tl-unx.tar.gz | tar xf -
+
+pushd install-tl-*
+
+perl ./install-tl -repository ftp://tug.org/historic/systems/texlive/${TL_VERSION}/tlnet-final -no-doc-install -no-src-install -no-interaction
+
+popd
+
+rm -rf texlive.profile install-tl-*
+
+popd
