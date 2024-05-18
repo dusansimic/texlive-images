8f040d0f629d2f36f6258b2a91f7441ab46ca0b2
Dušan Simić <dusan.simic1810@gmail.com>
2024-05-18 14:21:38 +0200

    fix: Try recommended extraction method

--- scripts/install_tl.sh
+++ scripts/install_tl.sh
@@ -16,7 +16,7 @@ cd /tmp
 
 curl -L -o install-tl-unx.tar.gz https://texlive.info/historic/systems/texlive/${TL_VERSION}/install-tl-unx.tar.gz
 mkdir tl
-tar -xzf install-tl-unx.tar.gz -C tl --strip-components=1
+zcat < install-tl-unx.tar.gz | tar xf -C tl --strip-components=1 -
 
 cd /tmp/tl
 
